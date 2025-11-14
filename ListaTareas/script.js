1823f3893c3939817ac48a9f58b17d6e632b814b
Dest-1 <72641450+Dest-1@users.noreply.github.com>
2025-11-14 11:05:52 -0500

    Update

--- ListaTareas/script.js
+++ ListaTareas/script.js
@@ -1,17 +1,39 @@
 const tarea = document.getElementById("tarea")
 const fecha = document.getElementById("fecha")
 const tareas = document.getElementById("lista_tareas")
+let lista_tareas = []
 
+if(localStorage.getItem("lista_tareas")){
+    lista_tareas = JSON.parse(localStorage.getItem("lista_tareas"))
+}
+
+function mostrar_fecha(fecha){
+
+    //Generar fecha
+    const fecha_titulo = new Date(Date.parse(fecha))
+
+    const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    };
+
+    fecha_mostrar = fecha_titulo.toLocaleDateString(undefined, options);
+    return fecha_mostrar
+
+}
     
 
 function guardar_tarea(){
+    
+    
     let txt_tarea = tarea.value
     let txt_fecha = fecha.value
 
     let fecha_actual = new Date()
     fecha_actual.getDate()
     let fecha_tarea = Date.parse(txt_fecha)
-    
 
 
     if(txt_tarea == "" || txt_fecha == null || txt_fecha == ""){
@@ -22,13 +44,18 @@ function guardar_tarea(){
         alert("No se pueden crear tareas para fechas pasadas")
         
         }else{
-            // fecha_tarea.Date.toLocaleDateString(undefined, options)
-            // console.log(fecha_tarea)
+
+            //Crear objeto en localstorage
+            let objeto_tarea = {"fecha": txt_fecha, "tarea": txt_tarea}
+            lista_tareas.push(objeto_tarea)
+            localStorage.setItem("lista_tareas", JSON.stringify(lista_tareas))
+
+            //Mostrar elementos en pantalla
             const nueva_tarea = document.createElement("li")
 
             nueva_tarea.innerHTML = `<li>
                                     <i class="far fa-circle co" data="realizado" id="0"></i>
-                                    <p>${fecha_tarea}: ${txt_tarea}</p>
+                                    <p>${mostrar_fecha(txt_fecha)}: ${txt_tarea}</p>
                                     <i class="fas fa-trash de" data="eliminado" id="0"></i>
                                 </li>`
             tareas.appendChild(nueva_tarea)
@@ -38,4 +65,20 @@ function guardar_tarea(){
     }
     
 
+}
+
+function cargar_tareas(){
+ 
+
+    for(let mi_tarea of lista_tareas){
+        const nueva_tarea = document.createElement("li")
+
+            nueva_tarea.innerHTML = `<li>
+                                    <i class="far fa-circle co" data="realizado" id="0"></i>
+                                    <p>${mostrar_fecha(mi_tarea.fecha)}: ${mi_tarea.tarea}</p>
+                                    <i class="fas fa-trash de" data="eliminado" id="0"></i>
+                                </li>`
+            tareas.appendChild(nueva_tarea)
+            console.log(lista_tareas)
+    }
 }
